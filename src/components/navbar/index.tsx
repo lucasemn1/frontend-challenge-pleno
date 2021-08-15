b35520019cac7c03038a509ada69a52ece457e32
Lucas Nóbrega <lucasnobrega.js@gmail.com>
2021-08-15 02:43:51 -0300

    Preparando a navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,3 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./styles.module.scss";
+
 export default function Navbar() {
-  return <nav></nav>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={`content ${styles.content}`}>
+        <Image
+          src="/assets/img/logo.png"
+          alt="Logomarca"
+          className={styles.logo}
+          width="90"
+          height="28"
+        />
+
+        <span>
+          <ul>
+            <li>
+              <Link href="#">Sobre</Link>
+            </li>
+            <li>
+              <Link href="#">Preço</Link>
+            </li>
+            <li>
+              <Link href="#">Desenvolvedores</Link>
+            </li>
+          </ul>
+
+          <ul>
+            <li>Buscar</li>
+            <li>
+              <Link href="#">Ajuda</Link>
+            </li>
+            <li>
+              <Link href="#">Contato</Link>
+            </li>
+            <li className={styles.start}>
+              <Link href="#">Começar</Link>
+            </li>
+          </ul>
+        </span>
+      </div>
+    </nav>
+  );
 }
