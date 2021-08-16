a1c30122c341f0bff13637ca8f552c6be7033a23
Lucas Nóbrega <lucasnobrega.js@gmail.com>
2021-08-16 03:52:11 -0300

    Preparando a responsividade da navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,24 @@
+// Next
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
+// Icons
+import { MdMenu } from "react-icons/md";
+
+// Styles
 import styles from "./styles.module.scss";
 
 export default function Navbar() {
+  function toggleMenu() {
+    const navMenu = document.getElementById("nav-menu");
+
+    if (window.screen.width <= 960) {
+      navMenu.classList.toggle(styles.hiddenMobileMenu);
+      console.log("oi");
+    }
+  }
+
   return (
     <nav className={styles.navbar}>
       <div className={`content ${styles.content}`}>
@@ -15,7 +30,7 @@ export default function Navbar() {
           height="28"
         />
 
-        <span>
+        <span id="nav-menu" className={styles.hiddenMobileMenu}>
           <ul>
             <li>
               <Link href="#">Sobre</Link>
@@ -41,6 +56,10 @@ export default function Navbar() {
             </li>
           </ul>
         </span>
+
+        <button onClick={toggleMenu}>
+          <MdMenu size={20} />
+        </button>
       </div>
     </nav>
   );
